3c3df19adcd2c57cf1be56f102446a70599dde97
GOWTHAM KUMAR V <89139024+Gowtham212088@users.noreply.github.com>
2023-03-20 23:23:11 +0530

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,3 +1,3 @@
-export const Api = "http://localhost:7000";
+// export const Api = "http://localhost:7000";
 
-// export const Api = "https://interview-task-backend-d28r7llm3-gowtham212088.vercel.app";
+export const Api = "https://interview-task-backend-d28r7llm3-gowtham212088.vercel.app";
