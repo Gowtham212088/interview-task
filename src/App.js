6eb39ac983cd5b85648fc9325caee57566918470
GOWTHAM KUMAR V <89139024+Gowtham212088@users.noreply.github.com>
2023-03-21 00:55:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,33 +5,59 @@ import TopNavbar from "./Components/TopNavbar";
 import Dashboard from "./Components/Dashboard";
 import { useState } from "react";
 function App() {
-
   const [users, getUsers] = useState([]);
-console.log(users)
-const [loginResponse, setLoginResponse] = useState({});
-
-const filterUser = users.filter((e)=>{
-  return e.name !== loginResponse
-})
-console.log(filterUser);
+  console.log(users);
+  const [loginResponse, setLoginResponse] = useState({});
+  console.log(loginResponse);
+  const filterUser = users.filter((e) => {
+    return e.name !== loginResponse;
+  });
+  console.log(filterUser);
   return (
     <>
       <Routes>
-        <Route path="/" exact element={<LoginAndSignUp />} title="login" />
-        <Route path="/login" exact element={<LoginAndSignUp
-loginResponse={loginResponse} 
-setLoginResponse={setLoginResponse}
-        />} title="login" />
-        <Route path="/dashboard" exact element={<Dashboard
-        filterUser={filterUser}
-loginResponse={loginResponse} 
-setLoginResponse={setLoginResponse}
-        users={users}
-        getUsers={getUsers}
-        />} title="login" />
+        <Route
+          path="/"
+          exact
+          element={
+            <LoginAndSignUp
+              loginResponse={loginResponse}
+              setLoginResponse={setLoginResponse}
+            />
+          }
+          title="login"
+        />
+        <Route
+          path="/login"
+          exact
+          element={
+            <LoginAndSignUp
+              loginResponse={loginResponse}
+              setLoginResponse={setLoginResponse}
+            />
+          }
+          title="login"
+        />
+        <Route
+          path="/dashboard"
+          exact
+          element={
+            <Dashboard
+              filterUser={filterUser}
+              loginResponse={loginResponse}
+              setLoginResponse={setLoginResponse}
+              users={users}
+              getUsers={getUsers}
+            />
+          }
+          title="login"
+        />
       </Routes>
     </>
   );
 }
+
+export default App;
+
 
 export default App;
